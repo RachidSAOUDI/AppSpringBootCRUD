ca539294d3488a8c8d060a8d04fb741d6cc93b15
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2022-10-03 10:08:29 +0100

    initital commit

---
+++ src/main/java/ma/usmba/appspringcrud/AppSpringCrudApplication.java
@@ -0,0 +1,13 @@
+package ma.usmba.appspringcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpringCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppSpringCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/usmba/appspringcrud/AppSpringCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.usmba.appspringcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSpringCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
